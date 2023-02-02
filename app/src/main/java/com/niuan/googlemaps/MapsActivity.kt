ad2a395a08865fa1f5da7efc53d5beef184d1d1d
Niuan Lucas <niuanlucasnicolau@gmail.com>
2023-02-02 00:31:21 -0300

    Work Location

---
+++ app/src/main/java/com/niuan/googlemaps/MapsActivity.kt
@@ -0,0 +1,49 @@
+package com.niuan.googlemaps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.niuan.googlemaps.databinding.ActivityMapsBinding
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        mMap.isMyLocationEnabled = true;
+        mMap.uiSettings.isMyLocationButtonEnabled = true
+
+        val buttonMap = findViewById<FloatingActionButton>(R.id.buttonMap)
+        buttonMap.setOnClickListener{
+            val sydney = LatLng(mMap.myLocation.latitude, mMap.myLocation.longitude)
+            mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        }
+
+        //val sydney = LatLng(-34.0, 151.0)
+
+    }
+
+
+
+}
