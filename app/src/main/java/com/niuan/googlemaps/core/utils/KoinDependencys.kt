5a9fd7492784634b34b05eb41c909d944a0afd0f
Arthur Rodrigues <arthurrodrigues29@gmail.com>
2023-02-05 21:48:19 -0300

    acertando dependencias

--- app/src/main/java/com/niuan/googlemaps/core/utils/KoinDependencys.kt
+++ app/src/main/java/com/niuan/googlemaps/core/utils/KoinDependencys.kt
@@ -1,6 +1,8 @@
 package com.niuan.googlemaps.core.utils
 
+import com.google.gson.Gson
 import com.niuan.googlemaps.core.helper.SharedPreferenceHelper
+import com.niuan.googlemaps.data.data_source.LocalDataSource
 import com.niuan.googlemaps.data.data_source.LocalDataSourceImpl
 import com.niuan.googlemaps.data.mappers.PolygonsToStringMapper
 import com.niuan.googlemaps.data.mappers.StringToPolygonsMapper
@@ -8,14 +10,18 @@ import com.niuan.googlemaps.data.repositories.RepositoryImpl
 import com.niuan.googlemaps.domain.repositories.Repository
 import com.niuan.googlemaps.domain.usecases.GetPolygons
 import com.niuan.googlemaps.domain.usecases.SavePolygons
+import com.niuan.googlemaps.presentation.maps.MapsViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val modules = module {
+    single {Gson()}
     single { SharedPreferenceHelper(get()) }
-    single { LocalDataSourceImpl(get()) }
+    single<LocalDataSource> { LocalDataSourceImpl(get()) }
     single { PolygonsToStringMapper(get()) }
     single { StringToPolygonsMapper(get()) }
-    single { RepositoryImpl(get(), get(), get()) }
     single { GetPolygons(get()) }
     single { SavePolygons(get()) }
+    single<Repository> { RepositoryImpl(get(), get(), get()) }
+    viewModel {MapsViewModel(get(), get())}
 }
